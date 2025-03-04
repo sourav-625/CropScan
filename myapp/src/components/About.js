d1181d4e9ac6a28a14f004a60ecc9f7029fc9cf4
Om Singh <ninjaplays792020@gmail.com>
2025-03-05 00:06:43 +0530

    created the react app and folders for use

---
+++ myapp/src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <>
+    <div className="container">
+        <h1>About Section</h1>
+        <p>Here we will explain our project.</p>
+    </div>
+    </>
+  )
+}
